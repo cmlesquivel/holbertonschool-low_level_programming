26a6fdfb9b4001d17d013cce1892478aefc3234c
Juan Camilo <anayacamilo@yahoo.es>
2019-09-18 22:48:44 +0000

    reply point five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main -entry point
+ *
+ *return :Always 0 (success)
+ */
+
+int main(void)
+{
+char str[80] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s \n", str);  
+return (0);
+}
