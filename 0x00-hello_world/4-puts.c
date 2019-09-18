a3e6f44b10ccb184911896492576369e6d4e1c5d
Juan Camilo <anayacamilo@yahoo.es>
2019-09-18 22:03:18 +0000

    reply four point

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-  char str1[50];
-  strcpy(str1, "\"Programming is like building a multilingual puzzle");
-  puts(str1);   
-  return(0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str1[50];
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+puts(str1);
+return (0);
 }
