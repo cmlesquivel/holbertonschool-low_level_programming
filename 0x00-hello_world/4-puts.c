94a2105f1521523975b43c9a57a67c928aaa01d9
Juan Camilo <anayacamilo@yahoo.es>
2019-09-18 21:22:24 +0000

    reply point four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 int main () {
   char str1[50];
 
-  strcpy(str1, '"Programming is like building a multilingual puzzle');
+  strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
 
   puts(str1);
    
