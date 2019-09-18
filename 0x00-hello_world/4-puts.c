5257c09a41fd20938f90a519e1e11f7cf90f9d5b
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-09-18 16:28:04 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,7 @@
 
 int main () {
   char str1[50];
-
-  strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
-
-  puts(str1);
-   
+  strcpy(str1, "\"Programming is like building a multilingual puzzle");
+  puts(str1);   
   return(0);
 }
