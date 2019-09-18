e50d9742797606d2b3823447452e99a4df8921b2
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-09-18 18:01:48 -0500

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-char str[80] = "with proper grammar, but the outcome is a piece of art, \n";
-printf("%s", str);
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
