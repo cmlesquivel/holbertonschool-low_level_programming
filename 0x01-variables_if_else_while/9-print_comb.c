4c0bafe26f58bfdf1800efe1b5c9bc2e63f22f52
Juan Camilo <anayacamilo@yahoo.es>
2019-09-20 00:01:13 +0000

    reply nine point

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,13 @@ int i;
 for (i = 48; i < 58; i++)
 {
 putchar(i);
-if (i < 57)
+if (i == 57)
 {
-putchar(44);
-putchar(32);
-}
-else
-{
-putchar(36);
+putchar('\n');
 break;
 }
+putchar(44);
+putchar(32);
 }
 return (0);
 }
