e3860ddcf0a9e89497fa68b24fee27a2017050d5
cmlesquivel <anayacamilo@yahoo.es>
2019-09-26 01:20:09 +0000

    reply point eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,99 +12,100 @@ void print_negative(int n);
 
 void print_to_98(int n)
 {
-       
-        if (n > 98)
-        {
-            print_positive(n);
-        }
-        else if (n == 98)
-        {
-                printf("98\n");
-        }
-        else
-        {
-		print_negative(n);
-	}
+if (n > 98)
+{
+print_positive(n);
+}
+else if (n == 98)
+{
+printf("98\n");
+}
+else
+{
+print_negative(n);
+}
 }
 
-void print_positive(int n){
+void print_positive(int n)
+{
 
-	int a,b,c,d,e;
-	
-                    for (a = n; a >= 98; a--)
-                    {
-                    if (a > 99)
-                        {
-                                b = a / 100;
-                                c = a % 100;
-                                d = c / 10;
-                                e = c % 10;
-                                _putchar(b + '0');
-                                _putchar(d + '0');
-                                _putchar(e + '0');
-                        }
+int a, b, c, d, e;
 
-                        else{
-                                b = a / 10;
-                                c = a % 10;
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-                        }
-
-                        if (a != 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        else
-                        {
-                                _putchar('\n');
-                        }
-                }
-    
-
+for (a = n; a >= 98; a--)
+{
+if (a > 99)
+{
+b = a / 100;
+c = a % 100;
+d = c / 10;
+e = c % 10;
+_putchar(b + '0');
+_putchar(d + '0');
+_putchar(e + '0');
 }
 
-void print_negative(int n){
-	int a,b,c,d,e;	
-	       for (a = n; a <= 98; a++)
-                {
-                          if(a<0){                        
-                         if (a < -99)
-                        {
-                                b = -a / 100;
-                                c = a % 100;
-                                d = -c / 10;
-                                e = -c % 10;
-                                _putchar('-');
-                                _putchar(b + '0');
-                                _putchar(d + '0');
-                                _putchar(e + '0');
-                        }
-                        else
-                        {
-                                b = -a / 10;
-                                c = -a % 10;
-                                _putchar('-');
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-                        }
-                    }
-                    else
-                    {
-                                b = a / 10;
-                                c = a  % 10;
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-                    }
-                        if (a != 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        else
-                        {
-                                _putchar('\n');
-                        }
-                } 
+else
+{
+b = a / 10;
+c = a % 10;
+_putchar(b + '0');
+_putchar(c + '0');
+}
+if (a != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
+}
+}
+
+void print_negative(int n)
+{
+int a, b, c, d, e;
+
+for (a = n; a <= 98; a++)
+{
+if (a < 0)
+{
+if (a < -99)
+{
+b = -a / 100;
+c = a % 100;
+d = -c / 10;
+e = -c % 10;
+_putchar('-');
+_putchar(b + '0');
+_putchar(d + '0');
+_putchar(e + '0');
+}
+else
+{
+b = -a / 10;
+c = -a % 10;
+_putchar('-');
+_putchar(b + '0');
+_putchar(c + '0');
+}
+}
+else
+{
+b = a / 10;
+c = a  % 10;
+_putchar(b + '0');
+_putchar(c + '0');
+}
+if (a != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('\n');
+}
+}
 }
