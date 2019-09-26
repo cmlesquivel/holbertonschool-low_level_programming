3ed77349beb9f0df56a2318a8066f0f26d6b4847
cmlesquivel <anayacamilo@yahoo.es>
2019-09-26 03:05:44 +0000

    reply point twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,24 @@
 #include "holberton.h"
-
 /**
 * times_table - printer times  table
-*
 * Return: printer times table
 **/
-
 void print_times_table(int n)
 {
-if( n > 0 && n < 15){
-
+if (n > 0 && n < 15)
+{
 int i, x, answer;
 for (i = 0; i <= n; i++)
 {
 for (x = 0; x <= n; x++)
 {
 answer = i * x;
-if (answer >99)
+if (answer > 99)
 {
 _putchar(answer / 100 + '0');
-_putchar((answer % 100)/10 + '0');
+_putchar((answer % 100) / 10 + '0');
 _putchar(answer % 10 + '0');
 }
-
-
-
 else if (answer > 9)
 {
 _putchar(32);
