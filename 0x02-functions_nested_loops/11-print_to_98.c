85f5401c50a50dd3f36b23b1b9f9556344b9ae3d
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-09-25 19:31:46 -0500

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "stdio.h"x
 #include "holberton.h"
-void print_t(int n);
+void print_positive(int n);
 
 /**
 * print_to_98 - print a serie of numbers to 98
@@ -11,52 +11,23 @@ void print_t(int n);
 
 void print_to_98(int n)
 {
-	int a, b, c, d, e;
+        int a, b, c, d, e;
 
-	if (n > 98)
-	{
-		for (a = n; a >= 98; a--)
-		{
-		    if (a > 99)
-                        {
-                                b = a / 100;
-                                c = a % 100;
-                                d = c / 10;
-                                e = c % 10;
-                                _putchar(b + '0');
-                                _putchar(d + '0');
-                                _putchar(e + '0');
-                        }
+        if (n > 98)
+        {
+            print_positive(n);
+        }
+        else if (n == 98)
+        {
+                printf("98\n");
+        }
+        else
+        {
+                for (a = n; a <= 98; a++)
+                {
+                          if(a<0){
+
                         
-                        else{
-                                b = a / 10;
-                                c = a % 10;
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-                        }
-		    
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
-			}
-		}
-	}
-	else if (n == 98)
-	{
-		printf("98\n");
-	}
-	else
-	{
-		for (a = n; a <= 98; a++)
-		{
-			  if(a<0){
-                        
-                    
                          if (a < -99)
                         {
                                 b = -a / 100;
@@ -83,23 +54,54 @@ void print_to_98(int n)
                                 c = a  % 10;
                                 _putchar(b + '0');
                                 _putchar(c + '0');
-                        
-                    }
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
-			}
 
-		     
-		}
-	}
+                    }
+                        if (a != 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        else
+                        {
+                                _putchar('\n');
+                        }
+
+
+                }
+        }
 }
 
-void print_t(int n){
+void print_positive(int n){
+                    for (a = n; a >= 98; a--)
+                    {
+                    if (a > 99)
+                        {
+                                b = a / 100;
+                                c = a % 100;
+                                d = c / 10;
+                                e = c % 10;
+                                _putchar(b + '0');
+                                _putchar(d + '0');
+                                _putchar(e + '0');
+                        }
+
+                        else{
+                                b = a / 10;
+                                c = a % 10;
+                                _putchar(b + '0');
+                                _putchar(c + '0');
+                        }
+
+                        if (a != 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        else
+                        {
+                                _putchar('\n');
+                        }
+                }
+    
 
 }
