6180032a371ba43e6cd7db8be1fb78ff81e6e2ee
cmlesquivel <anayacamilo@yahoo.es>
2019-09-26 15:36:28 +0000

    reply point zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+
+/**
+ *_isupper return is the charaacter is mayus
+ * @n: chararter to check
+ *return: 1 if the character is mayus
+ */
+int _isupper(int n){
+	if(n>64 && n<91){
+		return (1);
+	}
+	else{
+		return(0);
+	}
+}
