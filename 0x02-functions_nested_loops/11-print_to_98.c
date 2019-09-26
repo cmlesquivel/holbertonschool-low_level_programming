6df03db89ce49e8b72f296371e57a526ca0bc587
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-09-25 19:43:44 -0500

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "holberton.h"
 void print_positive(int n);
+void print_negative(int n);
 
 /**
 * print_to_98 - print a serie of numbers to 98
@@ -23,52 +24,8 @@ void print_to_98(int n)
         }
         else
         {
-                for (a = n; a <= 98; a++)
-                {
-                          if(a<0){
-
-                        
-                         if (a < -99)
-                        {
-                                b = -a / 100;
-                                c = a % 100;
-                                d = -c / 10;
-                                e = -c % 10;
-                                _putchar('-');
-                                _putchar(b + '0');
-                                _putchar(d + '0');
-                                _putchar(e + '0');
-                        }
-                        else
-                        {
-                                b = -a / 10;
-                                c = -a % 10;
-                                _putchar('-');
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-                        }
-                    }
-                    else
-                    {
-                                b = a / 10;
-                                c = a  % 10;
-                                _putchar(b + '0');
-                                _putchar(c + '0');
-
-                    }
-                        if (a != 98)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        else
-                        {
-                                _putchar('\n');
-                        }
-
-
-                }
-        }
+		print_negative(n);
+	}
 }
 
 void print_positive(int n){
@@ -107,4 +64,48 @@ void print_positive(int n){
                 }
     
 
+}
+
+void print_negative(int n){
+	int a,b,c,d,e;	
+	       for (a = n; a <= 98; a++)
+                {
+                          if(a<0){                        
+                         if (a < -99)
+                        {
+                                b = -a / 100;
+                                c = a % 100;
+                                d = -c / 10;
+                                e = -c % 10;
+                                _putchar('-');
+                                _putchar(b + '0');
+                                _putchar(d + '0');
+                                _putchar(e + '0');
+                        }
+                        else
+                        {
+                                b = -a / 10;
+                                c = -a % 10;
+                                _putchar('-');
+                                _putchar(b + '0');
+                                _putchar(c + '0');
+                        }
+                    }
+                    else
+                    {
+                                b = a / 10;
+                                c = a  % 10;
+                                _putchar(b + '0');
+                                _putchar(c + '0');
+                    }
+                        if (a != 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        else
+                        {
+                                _putchar('\n');
+                        }
+                } 
 }
