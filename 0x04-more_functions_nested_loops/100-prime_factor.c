a2551596a5d695e771c0e2e14f797536cd2332ca
cmlesquivel <anayacamilo@yahoo.es>
2019-09-28 00:30:23 +0000

    reply point eleven

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - prints the largest prime factor of the number 612852475143,
-*
-* Return:prints the largest prime factor of the number 612852475143
-*/
+ * main - prints the largest prime factor of the number 612852475143,
+ *
+ * Return:prints the largest prime factor of the number 612852475143
+ */
 
 int main(void)
 {
-
 long number = 612852475143;
-long larges_prime = 2;
+long larges_prime = 1;
 long a;
 long reply = number;
 
@@ -22,6 +21,11 @@ reply = reply / a;
 if (larges_prime < a)
 {
 larges_prime = a;
+}
+a = 1;
+if (reply == 1)
+{
+break;
 }
 }
 }
