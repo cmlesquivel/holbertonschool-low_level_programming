f68b73a3693797f50e2282b08244a84135e00715
cmlesquivel <anayacamilo@yahoo.es>
2019-10-01 22:45:20 +0000

    reply point six

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* puts2 - print only characters multiples for two
+* @str: string to print
+* Return: void
+**/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+
+	}
+
+	_putchar('\n');
+
+}
