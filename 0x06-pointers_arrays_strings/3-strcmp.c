44587b23c5941164c7b27a402a472373f8a2a16e
cmlesquivel <anayacamilo@yahoo.es>
2019-10-03 23:27:04 +0000

    reply point three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,13 @@
+/**
+ * _strcmp - compare two strings
+ *@s1: string one
+ *@s2:string two
+ *Return: It returns an integer less than, equal to, or greater than zero if s1
+ * is found, respectively, to be less than, to match, or be greater than s2.
+ **/
+
+
+
 int _strcmp(char *s1, char *s2)
 {
 int i, difference;
