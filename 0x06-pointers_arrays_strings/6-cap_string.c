70114e44bbf8dd2a2046abc050ea08b73590ebc9
cmlesquivel <anayacamilo@yahoo.es>
2019-10-04 01:28:05 +0000

    reply point sirx

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+/**
+ * cap_string - capitalizes all words of a string
+ *@s: string to capitalized
+ *Return: string capitalized
+ **/
+
+char *cap_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] == ' ') || (s[i] == 9) || (s[i] == '\n') || (s[i] == ',') ||
+(s[i] == ';') || (s[i] == '.') || (s[i] == '!') || (s[i] == '?') ||
+(s[i] == '"') || (s[i] == '(') || (s[i] == ')') || (s[i] == '{') ||
+(s[i] == '}'))
+{
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] = (s[i + 1] - 32);
+}
+}
+}
+return (s);
+}
