bde60c486a63a7fe7e38e413f65eefda79d5493e
cmlesquivel <anayacamilo@yahoo.es>
2019-10-07 20:23:36 +0000

    reply point two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+* _strchr - locate a character in a string
+* @s: array
+* @c: character to search
+* Return: a pointer to the first occurrence of the c character
+**/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
