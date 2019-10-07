6a44cd7c3c77055c8c1550ce951793b8ab083893
cmlesquivel <anayacamilo@yahoo.es>
2019-10-07 20:43:22 +0000

    reply point two

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * _strchr - locate a character in a string
 * @s: array
@@ -15,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return ('\0');
+	return (NULL);
 }
