01dab118d2ed4a52883aadcac9dffcf7e18330af
cmlesquivel <anayacamilo@yahoo.es>
2019-10-13 17:48:16 +0000

    reply point two

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print the name of the file
+ * @argc: size the array argv
+ * @argv: array that contains the command line arguments
+ * Return: 0 is the compile is satisfactory
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
