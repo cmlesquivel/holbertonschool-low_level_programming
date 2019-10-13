01467511e6e8f188f769364f27d6653fc1a3ec57
cmlesquivel <anayacamilo@yahoo.es>
2019-10-13 16:41:08 +0000

    reply point zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - print the name of the file
+ * @argc: size the array argv
+ * @argv: array that contains the command line arguments
+ * Return: 0 is the compile is satisfactory
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
