887065dfc0acf5b7d2a7f4accb1ccec3eaec7ff6
cmlesquivel <anayacamilo@yahoo.es>
2019-10-15 21:34:46 +0000

    reply point zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *str_;
 	unsigned int i;
+
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	else
-	{
-		char *str_;
-
 		str_ = malloc(sizeof(char) * size);
 
 		for (i = 0; i < size; i++)
@@ -29,5 +27,4 @@ char *create_array(unsigned int size, char c)
 			str_[i] = c;
 		}
 		return (str_);
-	}
 }
