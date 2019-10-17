8e25d80f15c1214b742c8e9f830f539dcd310d2a
cmlesquivel <anayacamilo@yahoo.es>
2019-10-17 20:52:25 +0000

    reply point one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ int _strlen(char *s)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
+	char *str = NULL;
 	unsigned int size_s1 = 0;
 	unsigned int size_s2 = 0;
 	unsigned int i;
