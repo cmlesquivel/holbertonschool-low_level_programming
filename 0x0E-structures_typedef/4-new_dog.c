f3e31c31c68c2dad7b6886512f8b0f3222045355
cmlesquivel <anayacamilo@yahoo.es>
2019-10-22 02:26:12 +0000

    replay point 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-* init_dog - create a new dog
+* new_dog - create a new dog
 * @name: name to set
 * @age: age to set
 * @owner: owner to set
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+
 	my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
