8326ba7aeccd08e7efc5c591314ae4b464926c73
cmlesquivel <anayacamilo@yahoo.es>
2019-10-23 01:08:05 +0000

    reply point four

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,42 @@
 #include <stdlib.h>
 
 /**
+* _strlen - return the lenght of string
+* @s: string to count the length
+* Return: the lenght of string
+**/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+* _strcpy - copy the string src to string dest
+* @src: string source
+* @dest: string dest
+* Return: the string dest
+**/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x] = '\0';
+	return (dest);
+}
+
+/**
 * new_dog - create a new dog
 * @name: name to set
 * @age: age to set
@@ -13,6 +49,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	char *n_name;
+	char *n_owner;
 
 	my_dog = malloc(sizeof(dog_t));
 
@@ -20,9 +58,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	my_dog->name = name;
+	n_name = malloc(_strlen(name) + 1);
+
+	if (n_name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->name = _strcpy(n_name, name);
+
+	n_owner = malloc(_strlen(owner) + 1);
+	if (n_owner == NULL)
+	{
+		free(n_name);
+		return (NULL);
+	}
+	my_dog->owner = _strcpy(n_owner, owner);
+
 	my_dog->age = age;
-	my_dog->owner = owner;
 
 	return (my_dog);
 }
