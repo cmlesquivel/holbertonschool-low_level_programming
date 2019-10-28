ab9911843c7548dc1d2e9c6d2a7e613f770980c0
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-10-27 22:45:19 -0500

    Update 100-hello_holberton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-    msg db "Hello, Holberton",10
-
+    text db "Hello, Holberton",10
+ 
 section .text
     global _start
-
+ 
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
+    mov rsi, text
+    mov rdx, 14
     syscall
-    
+ 
     mov rax, 60
     mov rdi, 0
     syscall
