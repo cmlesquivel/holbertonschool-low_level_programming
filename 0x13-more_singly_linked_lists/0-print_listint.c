f43f48e5591512e0641b2c4c3b36867b5c02d830
cmlesquivel <anayacamilo@yahoo.es>
2019-11-05 14:26:27 +0000

    reply point zero

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,22 +2,6 @@
 #include <string.h>
 #include "lists.h"
 
-
-/**
- * print_number - print  digit number by digit number
- * @number: The number to print
- * Return: nothing
- */
-
-void print_number(int number)
-{
-	if (number > 10)
-	{
-		print_number(number / 10);
-	}
-	_putchar(number % 10 + '0');
-}
-
 /**
  * print_listint - function that prints all the elements of a listint_t list.
  * @h: element type listing_t
@@ -32,8 +16,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-	print_number(h->n);
-	_putchar('\n');
+		printf("%u\n", h->n);
 
 		h = h->next;
 		n++;
