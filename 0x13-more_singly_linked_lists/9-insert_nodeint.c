ab730931d1aaeef2a1ef677900d99b573b43e618
cmlesquivel <anayacamilo@yahoo.es>
2019-11-06 00:20:23 +0000

    reply point nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,31 @@ size_t listint_len(const listint_t *h)
 }
 
 /**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: address ot the pointer
+ * @n: number to add in the new list
+ * Return: a element type listint_t
+ *
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
+
+/**
  * insert_nodeint_at_index -function that insert a new node at a given position
  * @head: address ot the pointer
  * @index:is the index of the node to insert, starting at 0
@@ -33,17 +58,22 @@ size_t listint_len(const listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
 	listint_t *aux = NULL, *new = NULL;
-	unsigned int i = 0;
+	unsigned int i = 1;
 	size_t length_list = listint_len(*head) - 1;
 
-	if (index > length_list)
+	if (index > length_list || *head == NULL)
 	{
 		return (NULL);
 	}
 
 	aux = *head;
 
-	while (i < index - 1)
+	if (index == 0)
+	{
+		return (add_nodeint(head, n));
+	}
+
+	while (i < index)
 	{
 		aux = aux->next;
 		i++;
