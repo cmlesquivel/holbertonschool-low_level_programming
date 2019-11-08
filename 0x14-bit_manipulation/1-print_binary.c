23e1d815081acc9e197c341dbf439131923e99a9
cmlesquivel <anayacamilo@yahoo.es>
2019-11-08 20:55:42 +0000

    reply point one

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,48 +1,17 @@
-#include "holberton.h"
+#include <stdio.h>
+
 
 /**
  * print_binary - prints the binary representation of a number.
- * @a:number;
+ * @n:number;
  * Return: Always 0.
  */
 
-void print_binary(unsigned long int a)
+void print_binary(unsigned long int n)
 {
-	unsigned long int n = 0, b = 0;
-	unsigned long int acumulado = 0;
-	int aux = 0;
-
-	if (a == 0)
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-
-	while ((1 << n) <= a)
-		n++;
-
-	_putchar('1');
-	b = n - 1;
-	acumulado = 1 << b;
-	b--;
-
-	while (acumulado != a)
-	{
-		aux = 1 << b;
-
-		if ((acumulado + aux) > a)
-			_putchar('0');
-		else if ((acumulado + aux) == a)
-		{
-			_putchar('1');
-			_putchar('0');
-			acumulado = acumulado + aux;
-		}
-		else
-		{
-			_putchar('1');
-			acumulado = acumulado + aux;
-			}
-		b--;
-	}
+	putchar((n & 1) + '0');
 }
