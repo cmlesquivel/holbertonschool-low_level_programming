e68690e22da947c41337d0a198252b2b186906bd
cmlesquivel <anayacamilo@yahoo.es>
2019-11-08 21:39:11 +0000

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,9 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-int set_bit(unsigned long int *n, unsigned int index);
 
+int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
