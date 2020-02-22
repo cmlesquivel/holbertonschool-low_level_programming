a0de77e4da59ec4a020262ee77004aab2a9e1283
cmlesquivel <anayacamilo@yahoo.es>
2020-02-22 00:08:50 -0500

    task five

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+    """
+    return 12
